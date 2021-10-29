bcaec4229a027f552fa2a422515545241b128fa7
creodahn <justin@justindrew.net>
2021-10-29 15:31:29 -0600

    add mirage to avoid cors issues

---
+++ mirage/scenarios/default.js
@@ -0,0 +1,45 @@
+export default function(server) {
+
+  /*
+    Seed your development database using your factories.
+    This data will not be loaded in your tests.
+  */
+
+  console.log('seeding development db...');
+
+  // server.createList('post', 10);
+  server.db.loadData({
+    cptCodes: [
+      {
+        id: 3866,
+        description: 'Psychiatric Diagnostic Evaluation',
+        duration: 50,
+        rate: '100.0',
+        callToBook: false
+      },
+      {
+        id: 31822614,
+        description: 'Intro Call',
+        duration: 15,
+        rate: '0.0',
+        callToBook: false
+      }
+    ]
+  });
+
+  server.db.loadData({
+    offices: [
+      {
+        id: 8377723,
+        name: 'Video Office',
+        phone: '(626) 298-1956',
+        isVideo: true,
+        geolocation: {
+          lat: '0.0',
+          lng: '0.0'
+        },
+        isPublic: false
+      }
+    ]
+  })
+}
