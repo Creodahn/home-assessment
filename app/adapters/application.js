a6bbc13db356067f48be366dba9a0c608ceeb2d4
creodahn <justin@justindrew.net>
2021-10-29 11:45:34 -0600

    add adapter

---
+++ app/adapters/application.js
@@ -0,0 +1,13 @@
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import ENV from 'simplepractice-assessment/config/environnment';
+
+export default class ApplicationAdapter extends JSONAPIAdapter {
+  headers = {
+    Accept: 'application/vnd.api+json',
+    'Api-Version': '2020-01-01',
+    'Application-Build-Version': '0.0.1',
+    'Application-Platform': 'web'
+  };
+  host = ENV.APP.apiURL;
+  namespace = 'client-postal-api';
+}
